2d7c52094082d114ca8f8743878342e0d292fb9e
Prabhupad <prabhupad.swain@outlook.com>
2024-12-29 11:50:49 +0530

    Users fetched & rendered successfully from backend api

--- frontend/src/components/User/UsersList.jsx
+++ frontend/src/components/User/UsersList.jsx
@@ -1,9 +1,18 @@
-import users from "../../data.json";
+import { useContext, useEffect } from "react";
 
+import { UserContext } from "../../store/user/UserState.jsx";
 import { MDBRow, MDBCol, MDBContainer } from "mdb-react-ui-kit";
 import User from "./User.jsx";
 
+// import users from "../../data.json";
+
 const UsersList = () => {
+  const { users, getUsers } = useContext(UserContext);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   const listUsers = users.map((singleUser) => (
     <MDBCol key={singleUser.id} md="6">
       <User data={singleUser} />
